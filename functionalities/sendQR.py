953bddf9c6c3aef087d90ecf4ef51419923266dc
GhostOutpost <sohraabgaming@gmail.com>
2025-10-04 13:21:30 +0530

    updating sendQR.py to include QR code generation functionality

--- functionalities/sendQR.py
+++ functionalities/sendQR.py
@@ -1,31 +1,3 @@
-# import smtplib
-# import imghdr
-# from email.message import EmailMessage
-
-
-# def sendQR(studentID, studentEmail):
-#     Sender_Email = "akademixrecords@gmail.com"
-#     Reciever_Email = studentEmail
-
-#     Password = "jbafgvdpehdpsldn"
-
-#     newMessage = EmailMessage()
-#     newMessage['Subject'] = "Your Attendance QR Code"
-#     newMessage['From'] = Sender_Email
-#     newMessage['To'] = Reciever_Email
-#     newMessage.set_content(' ')
-
-#     with open(f"./QR/QR_STUDENT_{studentID}.png", 'rb') as f:
-#         image_data = f.read()
-#         image_type = imghdr.what(f.name)
-#         image_name = f.name
-
-#     newMessage.add_attachment(image_data, maintype='image',
-#                               subtype=image_type, filename=image_name)
-
-#     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-#         smtp.login(Sender_Email, Password)
-#         smtp.send_message(newMessage)
 import smtplib
 from email.message import EmailMessage
 from PIL import Image
